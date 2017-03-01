089e25ba4d1a4e62f31aefea11f47ffe3fcfeebf
Peter <peter@Tanna-Grunigs-MacBook.local>
2017-02-28 19:10:00 -0700

    Started a text based adventure game.

---
+++ Adventure.py
@@ -0,0 +1,29 @@
+
+from os import clear
+from time import sleep
+
+
+
+def loading():
+    print("A classic text based adventure game")
+    sleep(2)
+    system("clear")
+    print("Made by Dragon123098")
+    sleep(2)
+    system("clear")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+loading()
